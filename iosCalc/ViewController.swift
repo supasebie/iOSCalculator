f41fddd4e889b52f0687b21f2a60b706f8323d17
SupaSebie <supasebie@gmail.com>
2024-04-05 15:56:30 -0700

    Finished calculator layout a stack views

---
+++ iosCalc/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  iosCalc
+//
+//  Created by Sebastian on 4/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var calculatedValue = 0;
+    
+    @IBOutlet weak var NineIUButton: UIButton!
+    @IBOutlet weak var EightUIButton: UIButton!
+    @IBOutlet weak var SevenUIButton: UIButton!
+    
+    @IBOutlet weak var DisplayUIV: UIImageView!
+    @IBOutlet weak var DisplayView: UIView!
+    
+    @IBAction func ModUIButton(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
